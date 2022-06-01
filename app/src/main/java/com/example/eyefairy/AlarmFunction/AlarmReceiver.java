344c9f749bc8e0ce771b092392b80410d6eb7fc7
JinaHong <73168867+kaworu7405@users.noreply.github.com>
2022-06-01 17:40:42 +0900

    add channel sound 2022-06-01

--- app/src/main/java/com/example/eyefairy/AlarmFunction/AlarmReceiver.java
+++ app/src/main/java/com/example/eyefairy/AlarmFunction/AlarmReceiver.java
@@ -1,5 +1,6 @@
 package com.example.eyefairy.AlarmFunction;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.Notification;
@@ -11,6 +12,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioAttributes;
 import android.media.RingtoneManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -55,7 +57,8 @@ public class AlarmReceiver extends BroadcastReceiver {
         final NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(context,channelId)
                 .setSmallIcon(R.mipmap.ic_launcher).setDefaults(Notification.DEFAULT_ALL)
                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setAutoCancel(true)
                 .setContentTitle(eyedropName)
                 .setContentText("Time to put in " + eyedropName)
@@ -67,8 +70,15 @@ public class AlarmReceiver extends BroadcastReceiver {
         final NotificationManager notificationManager=(NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-            NotificationChannel channel=new NotificationChannel(channelId,"Channel human readable title",NotificationManager.IMPORTANCE_DEFAULT);
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build();
+
+            @SuppressLint("WrongConstant") NotificationChannel channel=new NotificationChannel(channelId,"Channel human readable title",NotificationManager.IMPORTANCE_HIGH);
+            channel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION), audioAttributes);
             notificationManager.createNotificationChannel(channel);
+
         }
 
         int id=(int)System.currentTimeMillis();
